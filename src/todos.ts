9c8fa0df52c0339c20776703cf779f87cb6f8ef7
HMyounus80 <younusm137@gamil.com>
2021-07-25 15:10:33 +0600

    todo-list-done

---
+++ src/todos.ts
@@ -0,0 +1,20 @@
+export const data = [
+  {
+    id: "0",
+    title: "Task 1",
+    task: "Orange",
+    isCompleted: false,
+  },
+  {
+    id: "1",
+    title: "Task 2",
+    task: "Apple",
+    isCompleted: false,
+  },
+  {
+    id: "2",
+    title: "Task 2",
+    task: "Mango",
+    isCompleted: false,
+  },
+];
